256aa7d2f859b10998af0c15cc92c6522a48cac1
Paweł Jastrzębski <paweljastrzebski96@gmail.com>
2018-12-01 09:34:30 +0100

    initial commit

---
+++ src/main/java/com/jastrzab/SDA_SpringApp_Demo/SdaSpringAppDemoApplication.java
@@ -0,0 +1,12 @@
+package com.jastrzab.SDA_SpringApp_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdaSpringAppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdaSpringAppDemoApplication.class, args);
+	}
+}
